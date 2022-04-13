696559352a51f66b5934eaf369d9a341ff89a5a4
szhao2095 <altair.lugia@gmail.com>
2022-04-13 14:43:31 -0400

    Added tb for mask serializer.

--- rtl/mask_serializer.sv
+++ rtl/mask_serializer.sv
@@ -4,7 +4,7 @@ module mask_serializer
     parameter OP_CHANNEL_WIDTH = 20,
     parameter stepSel0 = 16,   //=320/OP_CHANNEL_WIDTH 
     parameter stepSel1 = 32,   //=640/OP_CHANNEL_WIDTH
-    parameter stepSel2 = 54   //=1080/OP_CHANNEL_WIDTH
+    parameter stepSel2 = 54    //=1080/OP_CHANNEL_WIDTH
 )
 (
     input  [IP_CHANNEL_WIDTH-1:0] DIN,
@@ -13,7 +13,7 @@ module mask_serializer
     input  load,
     input [1:0] imageResolution,
     output logic [OP_CHANNEL_WIDTH-1:0] DOUT
-    );
+);
 
     logic [IP_CHANNEL_WIDTH-1:0] dint;
     // Load the data into internal register file and shift the data
@@ -23,15 +23,13 @@ module mask_serializer
         end else if (next==1) begin
             dint[IP_CHANNEL_WIDTH-1:0] <= {1'b0,dint[IP_CHANNEL_WIDTH-1:1]};
         end
-    end    
-
+    end
 
     // decode imageResolution
     logic [2:0] sel;
     assign sel[0] = ~imageResolution[0] & ~imageResolution[1];  //320
     assign sel[1] =  imageResolution[0] & ~imageResolution[1];  //640
     assign sel[2] = ~imageResolution[0] &  imageResolution[1];  //1080    
-
 
     genvar i;
     for(i=0;i<OP_CHANNEL_WIDTH;i=i+1) begin
