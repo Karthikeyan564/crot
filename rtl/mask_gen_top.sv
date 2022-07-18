c275655e716a663ebcbe3b7612b38997273a991a
Karthikeyan564 <karthijob564@gmail.com>
2022-07-18 09:28:09 -0400

    Resolved Naming Conflict

--- rtl/mask_gen_top.sv
+++ rtl/mask_gen_top.sv
@@ -16,13 +16,13 @@ module mask_gen_top
      AXI_BUS.Slave axi_slave
      );
 
-    import wide_alu_reg_pkg::wide_alu_reg2hw_t;
-    import wide_alu_reg_pkg::wide_alu_hw2reg_t;
+    import mask_gen_reg_pkg::mask_gen_reg2hw_t;
+    import mask_gen_reg_pkg::mask_gen_hw2reg_t;
 
     //Wiring Signals
     REG_BUS #(.ADDR_WIDTH(32), .DATA_WIDTH(32)) axi_to_regfile();
-    wide_alu_reg2hw_t reg_file_to_ip;
-    wide_alu_hw2reg_t ip_to_reg_file;
+    mask_gen_reg2hw_t reg_file_to_ip;
+    mask_gen_hw2reg_t ip_to_reg_file;
 
     axi_to_reg_intf #(
                       .ADDR_WIDTH(AXI_ADDR_WIDTH),
@@ -51,7 +51,7 @@ module mask_gen_top
     `REG_BUS_ASSIGN_FROM_RSP(axi_to_regfile, from_reg_file_rsp);
 
 
-    wide_alu_reg_top #(
+    mask_gen_reg_top #(
     .reg_req_t(reg_req_t),
     .reg_rsp_t(reg_rsp_t)
     ) i_regfile (
@@ -63,7 +63,7 @@ module mask_gen_top
                  .reg_req_i(to_reg_file_req),
                  .reg_rsp_o(from_reg_file_rsp),
 
-                 //Signals to wide_alu IP
+                 //Signals to mask gen IP
                  .reg2hw(reg_file_to_ip),
                  .hw2reg(ip_to_reg_file)
     );
